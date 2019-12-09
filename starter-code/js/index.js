d014fbbf53a1bb7f05c164d68672cf54faa66857
Luna Daniel <20207@columbushs.com>
2019-12-09 13:45:19 -0500

    done with 1.1

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,15 +4,28 @@ var $calc = document.getElementById('calc');
 function updateSubtot($product) {
   // Iteration 1.1
 
-  let unitPrice = Number($product.querySelector(".product .pu span").innerHTML);
-  let qty = Number($product.querySelector(".product .qty label input").value);
+  let unitPrice = Number($product.querySelector(".pu span").innerHTML);
+  let qty = Number($product.querySelector(".qty label input").value);
 
   let subTotal = unitPrice * qty;
 
   $product.querySelector(".subtot span").innerHTML = subTotal;
+  return subTotal;
 }
 
 function calcAll() {
+
+  let productCollection = $cart.getElementsByClassName("product");
+  let $product;
+  let sumSubtotal = 0;
+
+  for (let i = 0; i < productCollection.length; i++) {
+    $product = productCollection[i];
+    sumSubtotal += updateSubtot($product);
+  }
+
+  document.querySelector("h2 span").innerHTML = sumSubtotal;
+
   updateSubtot();
 }
 $calc.onclick = calcAll;
